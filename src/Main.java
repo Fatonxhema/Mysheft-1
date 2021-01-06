15c674df4ee378fcf874f7cef3871119c005a629
Fatonxhema <es43605@universum-ks.org>
2021-01-07 00:19:54 +0100

    my project

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-         Libri[] librat;
-        Autori[] autoret;
+        int index = 0;
+        Libri[] librat = new Libri[index++];
+        Autori[] autoret = new Autori[index++];
 
         Biblioteka b = new Biblioteka("Biblioteka e shkolles", 200);
         LibriShkollor l1 = new LibriShkollor(1244, "Hello World", 2020, 4, "SHK1");
@@ -20,39 +21,35 @@ public class Main {
         if (pergjigja == 0) {
             System.exit(0);
         }
-        if (pergjigja == 1) {
-            //permes titutllit
-            System.out.println("Ju lutem shkruani titullin e librit");
-            String titulli = sc.nextLine();
-
-        /*    for (Libri libri : librat) {
-
-                if (libri.equals(titulli)) {
-                    System.out.println("Lbri egziston ne libarri");
-                }
-                else {
-                    System.out.println("Nuk u gjet ne librari");
-                }
-            } */
-        }
-        if (pergjigja == 2) {
-                //permes autorit
-            System.out.println("Ju lutem shkruani llojin e autorit");
-            String autori = sc.nextLine();
-           /* for (Autori autor : autoret) {
-                if (autor.equals(autori)) {
-                    System.out.println("Autori ekziston!");
-                }
-                else{
-                    System.out.println("Nuk u gjet autori");
-                }
-            }*/
-        }
         else {
-                System.out.println("ju lutem shkruani njeren prej numrave te lartpermendur");
-                System.exit(0);
+            if (pergjigja == 1) {
+
+                System.out.println("Ju lutem shkruani titullin e librit");
+                String titulli = sc.nextLine();
+
+                for (Libri libri : librat) {
+
+                    if (libri.equals(titulli)) {
+                        System.out.println("Lbri egziston ne libarri");
+                    } else {
+                        System.out.println("Nuk u gjet ne librari");
+                    }
+                }
+            }
+            if (pergjigja == 2) {
+
+                System.out.println("Ju lutem shkruani llojin e autorit");
+                String autori = sc.nextLine();
+                for (Autori autor : autoret) {
+                    if (autor.equals(autori)) {
+                        System.out.println("Autori ekziston!");
+                    } else {
+                        System.out.println("Nuk u gjet autori");
+                    }
+                }
             }
 
         }
-
     }
+
+}
